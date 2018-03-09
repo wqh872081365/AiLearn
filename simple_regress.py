b75dede5c5f322d895b9b8c20dbd178ea11aaa2e
wangqihui <wangqihui0324@gmail.com>
2018-03-09 10:49:58 +0800

    simple demo

---
+++ simple_regress.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+
+x = tf.constant([[1], [2], [3], [4]], dtype=tf.float32)
+y_true = tf.constant([[0], [-1], [-2], [-3]], dtype=tf.float32)
+
+linear_model = tf.layers.Dense(units=1)
+
+y_pred = linear_model(x)
+loss = tf.losses.mean_squared_error(labels=y_true, predictions=y_pred)
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+tf.summary.FileWriter('.', sess.graph)
+
+for i in range(100):
+    _, loss_value = sess.run((train, loss))
+    print(loss_value)
